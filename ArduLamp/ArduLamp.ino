38c6dbbd9e4901266c12380a278f886eb338d82c
Manuel Zaforas <mzaforas@gmail.com>
2012-08-30 18:37:04 +0200

    Support to delayed turn off via numeric parameter

--- ArduLamp/ArduLamp.ino
+++ ArduLamp/ArduLamp.ino
@@ -8,26 +8,58 @@ char buf;
 int lamp_pin = 13;
 int countdown;
 boolean countdown_enable = false;
+char current_command;
+boolean waiting_data;
 
 void setup() { 
   pinMode(lamp_pin, OUTPUT);
   Serial.begin(9600);
 } 
 
-void loop() { 
-  if (Serial.available() > 0) { 
+/**
+ * Parse command
+ */
+void parseCommand() {
     buf = Serial.read();
+    Serial.println(buf); // debug
+    // turn off
     if (buf == 'f') { 
       digitalWrite(lamp_pin, LOW);
+      countdown_enable = false; // disable countdown because we have turned off now the lamp
     } 
+    // turn on
     if (buf == 'n') { 
       digitalWrite(lamp_pin, HIGH);
     }
+    // delayed turn off, but we need more data
     if (buf == 't') {
-      countdown_enable = true;
-      countdown = 600; // 10 minutos
+      // read time
+      current_command = buf;
+      waiting_data = true;
+    }
+}
+
+/**
+ * Parse data
+ */
+void parseData() {
+  byte d = Serial.read();
+  // delayed turn off
+  if (current_command == 't') {
+    countdown_enable = true;
+    countdown = (int)d * 60;
+  }
+}
+
+void loop() { 
+  if (Serial.available() > 0) { 
+    if (waiting_data) {
+      parseData();
+    } else {
+      parseCommand();
     }
   }
+  
   if (countdown_enable) {
     countdown--;
     if (countdown == 0) {
